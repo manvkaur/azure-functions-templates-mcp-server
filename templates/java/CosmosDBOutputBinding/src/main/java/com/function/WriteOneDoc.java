4521e8023185685c73c71e267067c86605852bc7
manvkaur <67894494+manvkaur@users.noreply.github.com>
2025-09-30 15:08:21 -0700

    use java version 12

--- templates/java/CosmosDBOutputBinding/src/main/java/com/function/WriteOneDoc.java
+++ templates/java/CosmosDBOutputBinding/src/main/java/com/function/WriteOneDoc.java
@@ -6,6 +6,7 @@ import com.microsoft.azure.functions.HttpRequestMessage;
 import com.microsoft.azure.functions.HttpResponseMessage;
 import com.microsoft.azure.functions.HttpStatus;
 import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.annotation.CosmosDBOutput;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
 
