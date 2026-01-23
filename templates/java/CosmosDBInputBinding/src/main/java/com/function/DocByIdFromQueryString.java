e6bd82e9801226db90bcfed5e5a0505968a72c29
manvkaur <67894494+manvkaur@users.noreply.github.com>
2026-01-23 11:56:32 -0800

    Clean up unused imports in Java templates
    - Remove unused AuthorizationLevel import from CosmosDBInputBinding template
    - Remove unused AuthorizationLevel import from BlobOutputBinding template

--- templates/java/CosmosDBInputBinding/src/main/java/com/function/DocByIdFromQueryString.java
+++ templates/java/CosmosDBInputBinding/src/main/java/com/function/DocByIdFromQueryString.java
@@ -5,7 +5,6 @@ import com.microsoft.azure.functions.HttpMethod;
 import com.microsoft.azure.functions.HttpRequestMessage;
 import com.microsoft.azure.functions.HttpResponseMessage;
 import com.microsoft.azure.functions.HttpStatus;
-import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.CosmosDBInput;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
